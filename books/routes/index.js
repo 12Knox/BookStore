0fe4ae3dba05734573c947c9728c00b3967fc927
A-SK <eisuke_nakanishi@12knox.com>
2017-10-02 14:47:10 +0900

    Delete and edit.

--- books/routes/index.js
+++ books/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// indexを取得
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
 
 module.exports = router;
