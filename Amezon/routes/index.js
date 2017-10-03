c418e509e5812122267c80dddbf0f14a7fc219c3
A-SK <eisuke_nakanishi@12knox.com>
2017-10-03 16:24:38 +0900

    Updated.

--- Amezon/routes/index.js
+++ Amezon/routes/index.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect('');
+const Schema = mongoose.Schema;
+const url = 'mongodb://localhost:27017/amezon';
 
 // indexを取得
 router.get('/', (req, res, next) => {
-  res.render('index', { hoge: 'Hahaha!' });
+  res.render('index', { hoge: 'Welcome!' });
 });
 
-router.get('/test/:id', (req, res, next) => {
-  res.render('test', { output: req.params.id });
+router.get('/get-data', (req, res, next) => {
+
 });
 
-router.post('/test/submit', (req, res, next) => {
+router.post('/insert', (req, res, next) => {
+  const item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  };
+});
+
+router.post('/update', (req, res, next) => {
+  const item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  };
   const id = req.body.id;
-  res.redirect('/test/' + id);
+});
+
+router.post('/delete', (req, res, next) => {
+
 });
 
 module.exports = router;
