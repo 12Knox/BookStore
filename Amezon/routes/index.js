eed3c183440e56041692108f091a85c9df747cfc
A-SK <eisuke_nakanishi@12knox.com>
2017-10-03 18:31:51 +0900

    Almost done.

--- Amezon/routes/index.js
+++ Amezon/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-mongoose.connect('');
+mongoose.connect('localhost:27017/amezon');
 const Schema = mongoose.Schema;
-const url = 'mongodb://localhost:27017/amezon';
+
+const userDataSchema = new Schema({
+  title: { type: String, required: true },
+  content: String,
+  author: String,
+}, { collection: 'user-data' });
+
+const UserData = mongoose.model('userData', userDataSchema);
 
 // indexを取得
 router.get('/', (req, res, next) => {
@@ -11,28 +18,40 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/get-data', (req, res, next) => {
-
+  UserData.find()
+    .then((doc) => {
+      res.render('index', { items: doc });
+    })
 });
 
-router.post('/insert', (req, res, next) => {
+router.post('/register', (req, res, next) => {
   const item = {
     title: req.body.title,
     content: req.body.content,
     author: req.body.author,
   };
+  const data = new UserData(item);
+  data.save();
+  res.redirect('/');
 });
 
 router.post('/update', (req, res, next) => {
-  const item = {
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-  };
-  const id = req.body.id;
+  UserData.findById(id, (err, doc) => {
+    if (err) {
+      console.error('エラー！記事が見つかりません！');
+    }
+    doc.title = req.body.title;
+    doc.content = req.body.content;
+    doc.author = req.body.author;
+    doc.save();
+  });
+  res.redirect('/');
 });
 
 router.post('/delete', (req, res, next) => {
-
+  const id = req.body.id;
+  UserData.findByIdAndRemove(id).exec();
+  res.redirect('index');
 });
 
 module.exports = router;
