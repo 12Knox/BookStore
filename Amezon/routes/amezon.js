78325beea0d50d46947f5ef071059416c7497eb7
A-SK <eisuke_nakanishi@12knox.com>
2017-10-04 18:38:02 +0900

    Adding Amazon API.

--- Amezon/routes/amezon.js
+++ Amezon/routes/amezon.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const monk = require('monk');
-const db = monk('localhost:27017/amezon');
+const OperationHelper = require('apac').OperationHelper;
+const cheerio = require('cheerio');
+ 
+let opHelper;
+let title;
+let $;
 
-router.get('/books', (req, res) => {
-  res.render('amezon');
+opHelper = new OperationHelper({
+    awsId:     'AKIAJKZSA3XIWTBEWEKA',
+    awsSecret: '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',
+    assocId:   'eisuke1212-22',
+    endPoint: 'ecs.amazonaws.jp'
 });
 
-module.exports = router;
+opHelper.execute('ItemSearch', {
+    'SearchIndex': 'Books',
+    'BrowseNode': 465610,
+    'Keywords': 'GIS',
+    'ResponseGroup': 'Small',
+    'Sort': 'salesrank'
+}, 
+
+(err, results, xml) => {
+    if (err) { console.log("error"); return; }
+    $ = cheerio.load(xml);
+    $("Items > Item").each(function(idx, item) {
+        title = $(item).find("Title").text();
+        console.log(title);
+    });
+});
